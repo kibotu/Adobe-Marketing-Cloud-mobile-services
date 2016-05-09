c646c47380434b7f16c6706d89ea4222938a4410
Jan Rabe <jan.rabe@exozet.com>
2016-05-09 17:48:36 +0200

    added keep file to have reference to libs

---
+++ app/src/main/java/net/kibotu/android/adobe/Keep.java
@@ -0,0 +1,15 @@
+package net.kibotu.android.adobe;
+
+import com.adobe.mobile.ADBScene7.S7CachedResource;
+import com.adobe.mobile.Analytics;
+
+/**
+ * Created by jan.rabe on 09/05/16.
+ */
+public class Keep {
+
+    public static void keep() {
+        final Class<?> analytics = Analytics.class;
+        final Class<?> aDBScene7 = S7CachedResource.class;
+    }
+}
